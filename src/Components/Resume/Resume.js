9420874657229d4e1a54586711f6868402660743
Johnny Jin <wesgur@gmail.com>
2020-08-19 23:35:55 +0900

    Resume section and stylesheets

---
+++ src/Components/Resume/Resume.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// import classNames from 'classnames';
+
+// import { default as Education } from './Education';
+// import { default as Experience } from './Expereince';
+// import { default as Skills } from './Skills';
+
+// import styles from './style.module.scss';
+
+// const Resume = () => {
+//     return (
+//         <div className={classNames(styles.Resume)}>
+            
+//         </div>        
+//     );
+// }
+
+// export default Resume;
---
+++ src/Components/Resume/index.js
@@ -0,0 +1,4 @@
+export { default } from './Resume';
+export { default as Education } from './Education';
+export { default as Experience } from './Experience';
+
