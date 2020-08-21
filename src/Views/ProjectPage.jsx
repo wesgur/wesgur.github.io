b0c155d4fc6b3c94f30b18625fd6a602a6bb45dc
Johnny Jin <wesgur@gmail.com>
2020-08-21 17:09:14 +0900

    Project page added

---
+++ src/Views/ProjectPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classNames from 'classnames';
+
+import { default as Page } from '../Components/Page';
+import { default as Project } from '../Components/Project';
+
+const ProjectPage = (props) => (
+    <Page>
+        <Project> </Project>
+    </Page>
+)
+
+export default ProjectPage;
