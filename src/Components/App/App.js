1428337b4872504bc42e9e57e36ac283f4dff233
Johnny Jin <wesgur@gmail.com>
2020-09-02 04:19:40 +0900

    Meta tags added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ import { default as AboutPage } from '../../Views/AboutPage';
 import { default as Navigation } from '../Navigation';
 import { default as Container } from '../Container';
 
-import style from './styles.module.scss';
+import styles from './styles.module.scss';
 import 'react-pdf/src/Page/AnnotationLayer.css';
 
 const title = "Johnny | Software Developer";
@@ -20,30 +20,35 @@ const keywords = [ "Personal Website", "Johnny" ];
 const author = "Dong Hyuk Jin"
 
 function App() {
-  return (
-    <div>
-      <Helmet>
-        <title> { title } </title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords.join(" ")}/>
-        <meta name="author" content={author}/>
-      </Helmet>
-      <Router>
-        <Navigation />
-        <div className={classNames(style.content)}>
-          <Container>
-            <Switch>
-              <Route exact path="/" component={ HomePage } />
-              <Route path="/about" component={ AboutPage } />
-              <Route path="/project" component={ ProjectPage } />
-              <Route path="/resume" component={ ResumePage } />
-              <Redirect to="/" />
-            </Switch>
-          </Container>
-        </div>
-      </Router>    
-    </div>
-  );
+	return (
+		<div className={classNames(styles.app)}>
+			<Helmet>
+				<title> { title } </title>
+				<meta name="description" content={description} />
+				<meta name="keywords" content={keywords.join(" ")}/>
+				<meta name="author" content={author}/>
+				
+				<meta property="og:title" content="Johnny's Personal Website"/>
+				<meta property="og:description" content="Welcome to Johnny's website. Feel free to contact me!"/>
+				<meta property="og:image" content="https://www.wesgur.com/screenshots/homepage.png"/>
+				<meta property="og:url" content="https://www.wesgur.com"/>
+			</Helmet>
+			<Router>
+				<Navigation />
+				<div className={classNames(styles.content)}>
+					<Container>
+						<Switch>
+							<Route exact path="/" component={ HomePage } />
+							<Route path="/about" component={ AboutPage } />
+							<Route path="/project" component={ ProjectPage } />
+							<Route path="/resume" component={ ResumePage } />
+							<Redirect to="/" />
+						</Switch>
+					</Container>
+				</div>
+			</Router>    
+		</div>
+	);
 }
 
 export default App;
