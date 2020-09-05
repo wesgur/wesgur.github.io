3b30b40c856a6af2fcd281266baf35e599af7a67
wesgur <wesgur@users.noreply.github.com>
2020-09-05 18:10:40 +0000

    deploy: e964756b7b7ccf0588cdbdb7e67973e5c038e849

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "wesgur.github.io/precache-manifest.aea621dbb26d6c555f338b383efe7729.js"
+  "/precache-manifest.0bc44e58321cfc9b1f447ad4d63096cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("wesgur.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
