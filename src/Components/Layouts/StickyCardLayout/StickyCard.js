8b390e433b549c9d0fe47dd6899c7c1443402b26
Johnny Jin <wesgur@gmail.com>
2020-11-02 12:56:42 -0500

    Removed unecessary comments

--- src/Components/Layouts/StickyCardLayout/StickyCard.js
+++ src/Components/Layouts/StickyCardLayout/StickyCard.js
@@ -145,19 +145,6 @@ export const StickyCard = (props) => {
                             <p> There is nothing to show in timeline </p>
                         )
                     }
-                {/* <Timeline>
-                    <TimelineEvent title="John Doe sent a SMS"
-                                createdAt="2016-09-12 10:06 PM">
-                        I received the payment for $543. Should be shipping the item within a couple of hours.
-                    </TimelineEvent>
-                    <TimelineEvent
-                        title="You sent an email to John Doe"
-                        createdAt="2016-09-11 09:06 AM">
-                        Like we talked, you said that you would share the shipment details? This is an urgent order and so I
-                            am losing patience. Can you expedite the process and pls do share the details asap. Consider this a
-                            gentle reminder if you are on track already!
-                    </TimelineEvent>
-                </Timeline> */}
                 </CardContent>
             </Collapse>
             </Card>
